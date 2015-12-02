2acef998c5254226cb1b3716cc07672ee5215ccd
Izzy <oladejo09@gmail.com>
2015-12-02 00:33:04 +0000

    fetching all tasks from database

---
+++ Fetch_Task/view_task.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+	<style>
+
+	</style>
+	<title>Index</title>
+	<link rel="stylesheet" href="css/style.css">
+	<script>
+	</script>
+
+<h1> View All Tasks </h1>
+
+</head>
+<body >
+
+<?php
+if(isset($_REQUEST['tn'])){
+    include_once("se_task.php");
+        $obj = new se_task();
+        if(!$obj->get_task()){
+            echo '{"result":0,"message":"failed to fetch data"}';
+            return;
+        }else{
+            $row=$obj->fetch();
+	       echo '{"result":1,"tasks":[';	/*start of json object*/
+	       while($row){
+		      echo json_encode($row);/*convert the result array to json object*/
+		      $row=$obj->fetch();
+		      if($row){
+			     echo ",";					/*if there are more rows, add comma*/
+		      }
+	       }
+	       echo "]}";
+        }
+}
+?>
+</table>
+</body>
+</html>
